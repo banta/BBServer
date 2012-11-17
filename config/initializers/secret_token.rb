512aa8ac76e8c89e426d2f4821ccdb9b42c2f00f
Bernard Banta <banta.bernard@gmail.com>
2012-11-17 16:58:01 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlackSlash::Application.config.secret_token = '027e93a7607328350f6188ee8a3c30b4bfa8a837a0c47a7ffc3bd26ea6530b51318c3e94233d9a6fd4657cb02ba2f3fa0175ec4e5a26fff2af645ed3c1fe63f1'
